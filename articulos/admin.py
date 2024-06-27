b5b31dd02fa20f8898a5b6bc73c79dfee1bcf35a
NicolasVasq <ni.vasquezc@duocuc.cl>
2024-06-27 00:52:06 -0400

    cambioxd

--- articulos/admin.py
+++ articulos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Marca, Producto
+
+@admin.register(Marca)
+class marcaAdmin(admin.ModelAdmin):
+    list_display=['idMarca','nombreMarca']
+    
+
+@admin.register(Producto)
+class productoAdmin(admin.ModelAdmin):
+    list_display=[' productoId','marca','imagen','precio']
 
 # Register your models here.
