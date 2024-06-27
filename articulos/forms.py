d105bdaf9a4ba4583ad5dd159da84a3267fa852f
NicolasVasq <ni.vasquezc@duocuc.cl>
2024-06-27 06:09:44 -0400

    cambiocrud

--- articulos/forms.py
+++ articulos/forms.py
@@ -1,13 +1,35 @@
 # forms.py
 from django import forms
-from .models import Marca, Producto
-
-class MarcaForm(forms.ModelForm):
-    class Meta:
-        model = Marca
-        fields = ['nombreMarca']
+from django.forms import widgets
+from .models import Producto
 
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
-        fields = ['productoId','nombre','imagen','precio','nombreMarca']
+        fields = ['nombre', 'imagen', 'precio', 'nombreMarca']  # No incluimos 'productoId'
+        labels = {
+            'nombre': 'Nombre Producto',
+            'imagen': 'Imagen del producto',
+            'precio': 'Precio del producto',
+            'nombreMarca': 'Marca del producto'
+        }
+        widgets = {
+            'nombre': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el nombre del producto',
+                'id': 'nombre'
+            }),
+            'precio': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el precio del producto',
+                'id': 'precio'
+            }),
+            'imagen': forms.FileInput(attrs={
+                'class': 'form-control',
+                'id': 'imagen'
+            }),
+            'nombreMarca': forms.Select(attrs={
+                'class': 'form-control',
+                'id': 'nombreMarca'
+            })
+        }
