8255db25275dfe2021b8a53080511269073e708c
MauricioGHUB <mau.gatica@duocuc.cl>
2024-07-14 18:42:43 -0400

    Traspaso 2

--- articulos/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Marca
-
-class MarcaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Marca
-        fields = ['idMarca', 'nombreMarca']
